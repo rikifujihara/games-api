28bedaed2587f8a5513ea0ad7f79a63f92eedc4a
Riki Fujihara <rikifujihara@gmail.com>
2024-03-29 01:03:53 +1100

    project setup

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,17 @@
+using GameStore.Api.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<GameDto> games =
+[
+    new(1, "Street Fighter II", "Fighting", 19.19M, new DateOnly(1992, 7, 1)),
+    new(2, "Tekken", "Fighting", 59.99M, new DateOnly(2001, 5, 4)),
+    new(3, "Call of Duty", "FPS", 16.19M, new DateOnly(2004, 6, 5))
+];
+
+app.MapGet("games", () => games);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
