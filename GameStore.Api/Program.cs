48223ce740385af5e8f6f583b283a4f8c5be631f
Riki Fujihara <rikifujihara@gmail.com>
2024-03-29 16:08:20 +1100

    remove generated files from version control

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,17 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+
+// connect to sqlite
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
